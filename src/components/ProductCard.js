afd48981ff40ea35c7639b48b342f60a04ce11b5
sireesha-dadi <sireeshaibm@gmail.com>
2024-12-16 14:02:27 +0530

    Updated

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,6 @@
 import "./ProductCard.css";
 export const ProductCard = ({product}) => {
-  const {id, name, price, image} = product;
+  const { name, price, image} = product;
 
   return (
     <div className="productCard">
